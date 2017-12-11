a390fd0c827320b32b72f637ac8a78e5d802e4ea
James <jamespfreed@gmail.com>
2017-12-11 14:49:05 -0700

    updated initial input
    Input now branches between integer and fraction

--- binaryConvert.py
+++ binaryConvert.py
@@ -1,46 +1,43 @@
+x = float(input('Enter an integer or a decimal between 0 and 1: '))
+orig = x
+
 # convert integers into binary
+if float(x)%1 == 0.0:
+    if int(x) < 0:
+        isNeg = True
+        x = abs(int(x))
+    else:
+        isNeg = False
+    result = ''
+    if int(x) == 0:
+        result = '0'
+    while int(x) > 0:
+        result = str(int(x)%2) + result
+        x = int(x) // 2
+        print(result)
+    print('The binary representation of the integer ' + str(int(orig)) + ' is ' + str(result))
+    if isNeg:
+        result = '-' + result
 
-num = int(input('Enter an integer: '))
-orig = num
-
-if num < 0:
-    isNeg = True
-    num = abs(num)
-else:
-    isNeg = False
-result = ''
-if num == 0:
-    result = '0'
-while num > 0:
-    result = str(num%2) + result
-    num = num // 2
-    print(result)
-print('The binary representation of the integer ' + str(orig) + ' is ' + str(result))
-if isNeg:
-    result = '-' + result
-"""
 
 # convert fractions into binary
+else:
+    p = 0
+    while ((2**p) * float(x)) % 1 != 0:
+        print('Remainder = ' + str((2**p) * float(x) - int((2**p) * float(x) )))
+        p += 1
 
-x = float(input('Enter a decimal between 0 and 1: '))
+    num = int(float(x) * (2**p))
 
-p = 0
-while ((2**p) * x) % 1 != 0:
-    print('Remainder = ' + str((2**p) * x - int((2**p) * x )))
-    p += 1
+    result = ''
+    if num == 0:
+        result = '0'
+    while num > 0:
+        result = str(num%2) + result
+        num = num//2
 
-num = int(x * (2**p))
+    for i in range(p - len(result)):
+        result = '0' + result
 
-result = ''
-if num == 0:
-    result = '0'
-while num > 0:
-    result = str(num%2) + result
-    num = num//2
-
-for i in range(p - len(result)):
-    result = '0' + result
-
-result = result[0:-p] + '.' + result[-p:]
-print('The binary representation of the decimal ' + str(x) + ' is ' + str(result))
-"""
+    result = result[0:-p] + '.' + result[-p:]
+    print('The binary representation of the decimal ' + str(float(x)) + ' is ' + str(result))
