604b1780496972c6e07c5fd789a57ae923d0f8fd
James <jamespfreed@gmail.com>
2017-12-11 15:15:43 -0700

    cleanup
    cleanup

--- binaryConvert.py
+++ binaryConvert.py
@@ -4,11 +4,10 @@ orig = x
 # convert integers into binary
 if float(x)%1 == 0.0:
     if int(x) < 0:
-        isNeg = True
         x = abs(int(x))
-    else:
-        isNeg = False
+
     result = ''
+
     if int(x) == 0:
         result = '0'
     while int(x) > 0:
@@ -16,28 +15,26 @@ if float(x)%1 == 0.0:
         x = int(x) // 2
         print(result)
     print('The binary representation of the integer ' + str(int(orig)) + ' is ' + str(result))
-    if isNeg:
-        result = '-' + result
-
 
 # convert fractions into binary
 else:
+    if float(x) < 0:
+        x = abs(float(x))
     p = 0
     while ((2**p) * float(x)) % 1 != 0:
         print('Remainder = ' + str((2**p) * float(x) - int((2**p) * float(x) )))
         p += 1
 
     num = int(float(x) * (2**p))
-
     result = ''
+
     if num == 0:
         result = '0'
     while num > 0:
         result = str(num%2) + result
         num = num//2
-
     for i in range(p - len(result)):
         result = '0' + result
 
     result = result[0:-p] + '.' + result[-p:]
-    print('The binary representation of the decimal ' + str(float(x)) + ' is ' + str(result))
+    print('The binary representation of the decimal ' + str(float(orig)) + ' is ' + str(result))
